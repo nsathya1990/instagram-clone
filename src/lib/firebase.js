b8168081b1cfd0bbcf9de378109f8e9fdeb6ced9
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-06-12 22:18:40 +0530

    create login routes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,7 +5,15 @@ import 'firebase/auth';
 /** Here I want to import the Seed File */
 /* import { seedDatabase } from '../seed'; */
 
-const config = {};
+const config = {
+    apiKey: 'AIzaSyCwbRqZDnqc6Vgcdmlu_fEJIdc3nd4g7gE',
+    authDomain: 'instagram-yt-effd1.firebaseapp.com',
+    databaseURL: 'https://instagram-yt-effd1-default-rtdb.firebaseio.com',
+    projectId: 'instagram-yt-effd1',
+    storageBucket: 'instagram-yt-effd1.appspot.com',
+    messagingSenderId: '22127523027',
+    appId: '1:22127523027:web:215edefd077b5505cf9026'
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
