9bd49a67d2beaca9a7ca7e5c18d8add42db734a0
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-06-13 10:30:13 +0530

    tailwind setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
 
+import './styles/app.css';
+
 ReactDOM.render(
     <FirebaseContext.Provider value={{ firebase, FieldValue }}>
         <App />
